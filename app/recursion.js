55abc74af60fdb1183d6684a2f3986204ba0ac7a
Juan Gomez <gomexa.10@gmail.com>
2019-03-06 11:25:35 -0500

    Solved all tests

--- app/recursion.js
+++ app/recursion.js
@@ -9,27 +9,31 @@ recursionAnswers = {
       }
    *
    * Where ... is the same type of object
-   * 
+   *
    * @param {fileSystemObject} data - a file system object as described above
    * @param {String} dirName - a directory name the files are desired to be listed from.
    * Note: This parameter is optional. If it is not provided, list ALL files.
-   * 
-   * @returns {Number[]} The files under the directory dirName, including subdiretories.
+   *
+   * @returns {String[]} The files under the directory dirName, including subdiretories.
    */
   listFiles: function listFiles(data, dirName) {
-
+    if (!dirName) {
+      const subFiles = data.subDirs.map(subDir => listFiles(subDir));
+      return [].concat.apply([...data.files], subFiles);
+    }
+    return listFiles(data.subDirs.filter(dir => dir.dirName === dirName)[0]);
   },
 
   /**
    * Determines the fibonacci number at position n.
    * https://en.wikipedia.org/wiki/Fibonacci_number
-   * 
+   *
    * The first few fibonacci numbers are: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
-   * 
+   *
    * @param {Number} n - the index of the fibonacci number desired
    * @returns {Number} The nth fibonacci number
    */
   fibonacci: function fibonacci(n) {
-
+    return n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
   },
 };
